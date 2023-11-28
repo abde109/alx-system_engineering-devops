8ab9f4169d8f5415b0df4b2146af3c163b7f06b3
abde109 <abderrahimkhadri2@gmail.com>
2023-11-29 00:26:31 +0100

    ast task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,31 @@
-class nginx_setup {
-    # Ensure nginx is installed
-    package { 'nginx':
-        ensure => installed,
-    }
+# Puppet manifest to install and configure nginx
 
-    # Start and enable nginx service
-    service { 'nginx':
-        ensure    => running,
-        enable    => true,
-        require   => Package['nginx'],
-    }
-
-    # Serve a custom homepage
-    file { '/var/www/html/index.html':
-        ensure  => file,
-        content => 'Hello World!',
-        require => Package['nginx'],
-        notify  => Service['nginx'],
-    }
-
-    # Configure nginx for redirect
-    file_line { 'nginx_redirect':
-        path    => '/etc/nginx/sites-available/default',
-        line    => 'rewrite ^/redirect_me https://www.example.com/ permanent;',
-        match   => '^rewrite ^/redirect_me',
-        require => File['/var/www/html/index.html'],
-        notify  => Service['nginx'],
-    }
-
-    # Restart nginx to apply changes
-    exec { 'nginx_restart':
-        command     => '/usr/sbin/service nginx restart',
-        refreshonly => true,
-    }
+# Ensure nginx is installed
+package { 'nginx':
+  ensure => installed,
 }
 
-include nginx_setup
+# Ensure nginx service is running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Create a custom homepage
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Configure nginx for redirect
+file_line { 'nginx_redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => File['/var/www/html/index.html'],
+  notify  => Service['nginx'],
+}
