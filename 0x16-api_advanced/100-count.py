765edff2ba55100e167666cb6bc200f867bc02d1
abde109 <abderrahimkhadri2@gmail.com>
2024-01-16 17:02:25 +0000

    Count it

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module for counting words in titles of subreddit's hot articles"""
+
+import requests
+
+
+def count_words(subreddit, word_list, after='', word_count={}):
+    if not after:
+        word_list = [word.lower() for word in word_list]
+        word_count = {word: 0 for word in word_list}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
+    headers = {"User-Agent": "CustomUserAgent/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get('data', {})
+    after = data.get('after')
+    hot_posts = data.get('children', [])
+
+    for post in hot_posts:
+        title = post.get('data', {}).get('title').lower()
+        for word in word_list:
+            word_count[word] += title.split().count(word)
+
+    if not after:
+        sorted_words = sorted(
+            [(count, word) for word, count in word_count.items() if count > 0], reverse=True)
+        for count, word in sorted_words:
+            print(f"{word}: {count}")
+        return
+
+    count_words(subreddit, word_list, after, word_count)
