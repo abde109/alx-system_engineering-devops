05f32e0af757ef2ce7889dfe9f72efe77184b7c3
abde109 <abderrahimkhadri2@gmail.com>
2024-01-16 16:22:41 +0000

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent': 'python:alx-system_engineering-devops:v1.0 (by /u/abde109)'}
+        'User-Agent': 'alx-system_engineering-devops:v1.0 (by /u/abde109)'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
