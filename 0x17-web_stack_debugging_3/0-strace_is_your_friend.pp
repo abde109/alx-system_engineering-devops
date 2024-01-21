0a73c8f19ed70465a68d32773bdae75c2587b85b
abde109 <abderrahimkhadri2@gmail.com>
2024-01-21 20:20:10 +0000

    latest tasks

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,27 @@
+# Puppet manifest to fix the WordPress settings file by replacing '.phpp' with '.php'
+
+exec { 'fix-wordpress-php-extension':
+  # Command to replace '.phpp' with '.php' using sed
+  command     => "/bin/sed -i 's/\\.phpp/\\.php/g' /var/www/html/wp-settings.php",
+
+  # Only run the command if '.phpp' is found in the file
+  onlyif      => "/bin/grep -q '\\.phpp' /var/www/html/wp-settings.php",
+
+  # Specify the command path
+  path        => ['/bin', '/usr/bin'],
+
+  # Execute this resource only when notified by another resource
+  refreshonly => true,
+
+  # Notify Apache service to restart if this exec is triggered
+  notify      => Service['apache2'],
+}
+
+# Service resource for managing the Apache service
+service { 'apache2':
+  # Ensure that Apache is running
+  ensure => running,
+
+  # Enable Apache to start on boot
+  enable => true,
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+# alx-system_engineering-devops
+0x17. Web stack debugging #3
