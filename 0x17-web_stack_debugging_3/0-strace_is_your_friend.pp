260e4a8e21e2a49786b1ce1370558cfbc8f27f34
abde109 <abderrahimkhadri2@gmail.com>
2024-01-21 20:27:51 +0000

    latest tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,13 @@
 # Puppet manifest to fix the WordPress settings file by replacing '.phpp' with '.php'
 
 exec { 'fix-wordpress-php-extension':
-  # Command to replace '.phpp' with '.php' using sed
-  command     => "/bin/sed -i 's/\\.phpp/\\.php/g' /var/www/html/wp-settings.php",
-
-  # Only run the command if '.phpp' is found in the file
-  onlyif      => "/bin/grep -q '\\.phpp' /var/www/html/wp-settings.php",
-
-  # Specify the command path
-  path        => ['/bin', '/usr/bin'],
-
-  # Notify Apache service to restart if this exec is triggered
-  notify      => Service['apache2'],
+  command => "/bin/sed -i 's/\\.phpp/\\.php/g' /var/www/html/wp-settings.php",
+  onlyif  => "/bin/grep -q '\\.phpp' /var/www/html/wp-settings.php",
+  path    => ['/bin', '/usr/bin'],
+  notify  => Service['apache2'],
 }
 
-# Service resource for managing the Apache service
 service { 'apache2':
-  # Ensure that Apache is running
   ensure => running,
-
-  # Enable Apache to start on boot
   enable => true,
 }
