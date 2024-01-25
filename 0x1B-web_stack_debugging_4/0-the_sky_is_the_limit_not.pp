44df03be9d1b3ef3ef1a189266461189cb99f654
abde109 <abderrahimkhadri2@gmail.com>
2024-01-25 16:38:48 +0000

    latest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,13 +3,11 @@
 # This is useful for high traffic sites.
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
-  onlyif  => 'grep -q "15" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx if the above exec is run.
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/',
-  refreshonly => true,
+  path    => '/etc/init.d/'
 }
